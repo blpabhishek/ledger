431a8771a44613811c096fb0b9c6372b2c7f4e85
Abhishek Gupta <blp.abhishek@yahoo.com>
2022-03-30 10:07:17 +0530

    Sets up the repo with gradle and junit

---
+++ src/test/java/com/example/LedgerTest.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class LedgerTest {
+    @Test
+    void thisIsBasicTest() {
+        Assertions.assertEquals(2,2);
+    }
+}
