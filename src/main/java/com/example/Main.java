57d0696aa99eb798599f6517cc767d9745e487f7
Abhishek Gupta <blp.abhishek@yahoo.com>
2022-04-02 11:16:36 +0530

    Dry run in main with actual end to end flow

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,11 @@
 package com.example;
 
+import com.example.domain.Ledgers;
+import com.example.domain.Status;
+import com.example.domain.UnknownUserException;
 import com.example.parser.Command;
 import com.example.parser.InputParser;
+import com.example.service.LedgerService;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -9,14 +13,14 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, UnknownUserException {
+        LedgerService ledgerService = new LedgerService(new Ledgers());
         List<String> fileContent = Files.readAllLines(Path.of("src/main/resources/transactions-1.txt"));
         for (String line : fileContent) {
             InputParser inputParser = new InputParser();
             Command command = inputParser.parse(line);
-            // create ledger service
-            // apply commands on ledger service
-            // get the result and show the response
+            Status status = ledgerService.execute(command);
+            System.out.println(status);
         }
     }
 
