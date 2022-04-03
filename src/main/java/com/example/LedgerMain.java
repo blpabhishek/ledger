d9731056641a6f25246e700060cd8f62f04923f7
Abhishek Gupta <blp.abhishek@yahoo.com>
2022-04-03 13:24:30 +0530

    rename the Main file as LedgerMain

--- src/main/java/com/example/LedgerMain.java
+++ src/main/java/com/example/LedgerMain.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
-public class Main {
+public class LedgerMain {
     public static void main(String[] args) {
         try {
             String filePath = args[0];
